fcc9b083ea9f958706d7610b6780c98305d84736
Kostya Bulaev <viphedin@gmail.com>
2020-08-18 15:08:10 +0400

    remove firebase service

--- src/android/src/firebase/FirebaseMessageReceiver.java
+++ src/android/src/firebase/FirebaseMessageReceiver.java
@@ -7,49 +7,40 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
-import android.util.Log;
 
 import androidx.annotation.NonNull;
 
-import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
 import ru.simdev.evo.video.R;
 import ru.simdev.livetex.FragmentEnvironment;
 import ru.simdev.livetex.utils.DataKeeper;
 
-public class FirebaseMessageReceiver extends FirebaseMessagingService {
+public class FirebaseMessageReceiver {
     private static final String TAG = "Livetex";
 
     public static final int iconColor = 0xFF4A47EC;
     public static final String CHANNEL_ID = "EvolifeChatService";
     public static boolean channelInited = false;
 
-    @Override
-    public void onMessageReceived(RemoteMessage remoteMessage) {
-        Log.d(TAG, remoteMessage.getFrom());
-        sendNotification(remoteMessage);
-    }
-
-    @Override
-    public void onNewToken(@NonNull String newToken) {
+    public static void saveToken(Context context, @NonNull String newToken) {
         // This token will start working on next Livetex init (app restart for now)
-        DataKeeper.saveRegId(this, newToken);
+        DataKeeper.saveRegId(context, newToken);
     }
 
-    private void sendNotification(RemoteMessage remoteMessage) {
+    public static void sendNotification(Context context, RemoteMessage remoteMessage) {
         if (remoteMessage.getNotification() == null) {
             return;
         }
 
-        Intent intent = new Intent(this.getApplicationContext(), FragmentEnvironment.class);
+        Intent intent = new Intent(context.getApplicationContext(), FragmentEnvironment.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_ONE_SHOT);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_ONE_SHOT);
 
         String messageTitle = remoteMessage.getNotification().getTitle();
         String messageText = remoteMessage.getNotification().getBody();
 
-        Notification.Builder notificationBuilder = new Notification.Builder(this)
+        Notification.Builder notificationBuilder = new Notification.Builder(context)
                         .setSmallIcon(R.drawable.icon)
                         .setContentTitle(messageTitle)
                         .setContentText(messageText)
@@ -61,12 +52,12 @@ public class FirebaseMessageReceiver extends FirebaseMessagingService {
         }
 
         if (Build.VERSION.SDK_INT >= 26) {
-            createNotificationChannel(this);
+            createNotificationChannel(context);
             notificationBuilder.setChannelId(CHANNEL_ID);
         }
 
         NotificationManager notificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 
         notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
     }
